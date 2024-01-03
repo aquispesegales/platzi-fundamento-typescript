88b245bb31b257093bc48bc9ac537de3f680615f
aquispesegales <alvaroquispesegales@gmail.com>
2024-01-03 00:32:10 -0400

    curso fundamentos type script video numbers terminado

---
+++ src/03-typing.ts
@@ -0,0 +1,12 @@
+(() =>{
+    let myProductName = 'Product 1';
+    let myProductPrice = 123;
+    
+    myProductName = 'change';
+    myProductName.toLowerCase();
+    
+    myProductPrice.toFixed();
+    
+    const myPorductStock = 1000;
+    const myProductName2 = 'Prodcut 1';
+})();
---
+++ src/04-numbers.ts
@@ -0,0 +1,20 @@
+(() => {
+    let productPrice = 100;
+    productPrice = 12;
+    console.log('productPrice ', productPrice);
+
+    let customerAge: number = 28;
+    customerAge = customerAge + 1;
+    console.log('customerAge ',customerAge);
+
+    let productInStock:number;
+    console.log('productInStock ',productInStock); 
+    if(productInStock>10){
+        console.log('is greater ');
+    }
+
+    let discount = parseInt('123');
+    console.log('discount ',discount);
+    
+
+})();
